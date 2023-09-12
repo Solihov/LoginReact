9e9b67ac2b03243ba47a7aa6a89eaadf66ac1462
Solihov <islomjonsolihov@gmail.com>
2023-09-12 15:18:31 +0500

    qwert

---
+++ LogPage/src/App.jsx
@@ -0,0 +1,11 @@
+import Login from './Pages/login'
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
---
+++ LogPage/src/Pages/login.jsx
@@ -0,0 +1,16 @@
+import './download.png'
+import './App.css'
+
+const Login = () => {
+    return (
+        <>
+            <div className="bodyBox">
+                <form action="">
+                    <img className="logo" src="./download.png" alt="png" />
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default Login
